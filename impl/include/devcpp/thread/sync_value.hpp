f1d53eb31f2bf3f8c21c0baa9e95af893e84c1b0
Jonathon Temelkovski <dev.jtemel@gmail.com>
2024-06-26 16:49:38 -0400

    Add operators to sync value

--- impl/include/devcpp/thread/sync_value.hpp
+++ impl/include/devcpp/thread/sync_value.hpp
@@ -18,11 +18,11 @@ class sync_ptr {
   sync_ptr(value_type& v, mutex_type& m) : m_mutex{m}, m_value{v} {
     m_mutex.lock();
   }
-  ~sync_ptr() {
-    m_mutex.unlock();
-  }
+  ~sync_ptr() { m_mutex.unlock(); }
 
   value_type& operator()() { return m_value; }
+  value_type& operator*() { return m_value; }
+  value_type* operator->() { return &m_value; }
 
  private:
   mutex_type& m_mutex;
@@ -36,10 +36,46 @@ class sync_value {
   using value_type = VALUE;
 
   sync_value() = default;
+  ~sync_value() = default;
+
+  sync_value(const sync_value& other) {
+    std::lock_guard<mutex_type> lock(other.m_mutex);
+    m_value = other.m_value;
+  }
+  sync_value(sync_value&& other) {
+    std::lock_guard<mutex_type> lock(other.m_mutex);
+    m_value = std::move(other.m_value);
+  }
+
   sync_value(value_type& v) : m_value{v} {}
+  sync_value(value_type&& v) : m_value{std::move(v)} {}
+
+  sync_value& operator=(const sync_value& other) {
+    if (this != &other) {
+      std::lock_guard<mutex_type> lock_this(m_mutex, std::defer_lock);
+      std::lock_guard<mutex_type> lock_other(other.m_mutex, std::defer_lock);
+      std::lock(lock_this, lock_other);
+      m_value = other.m_value;
+    }
+    return *this;
+  }
+
+  sync_value& operator=(sync_value&& other) noexcept {
+    if (this != &other) {
+      std::lock_guard<mutex_type> lock_this(m_mutex, std::defer_lock);
+      std::lock_guard<mutex_type> lock_other(other.m_mutex, std::defer_lock);
+      std::lock(lock_this, lock_other);
+      m_value = std::move(other.m_value);
+    }
+    return *this;
+  }
 
   sync_ptr<value_type, mutex_type> sync() {
     return sync_ptr<value_type, mutex_type>(m_value, m_mutex);
+  }
+
+  sync_ptr<value_type, mutex_type> operator->() {
+    return sync();
   }
 
  private:
